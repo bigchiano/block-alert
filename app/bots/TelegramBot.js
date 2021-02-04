f401aaae820cb0d38f58741f6c24c130d822b9e4
iamnotstatic <staticdev20046@gmail.com>
2021-02-04 12:18:25 +0100

    delete command

--- app/bots/TelegramBot.js
+++ app/bots/TelegramBot.js
@@ -57,17 +57,17 @@ bot.onText(/\/listAvailableCoins/, async (msg) => {
 
     let listCoins = ''
 
-    if (coins.length > 0) { 
+    if (coins.length > 0) {
         coins.forEach((coin) => {
             listCoins += `${capitalizeFirstLetter(coin.name)}: ${coin.symbol}\n`
         })
 
         bot.sendMessage(msg.chat.id, `${listCoins}`, { parse_mode: 'Markdown' })
     } else {
-        bot.sendMessage(msg.chat.id, `No available notifications, try again!`, { parse_mode: 'Markdown' })
+        bot.sendMessage(msg.chat.id, `No available notifications, try again!`, {
+            parse_mode: 'Markdown'
+        })
     }
-
-    
 })
 
 bot.onText(/\/listNotifications/, async (msg) => {
@@ -86,54 +86,82 @@ bot.onText(/\/listNotifications/, async (msg) => {
     let listCoins = ''
 
     if (notifications.length > 0) {
-
         notifications.forEach((notification) => {
-         listCoins += `${notification.coin.symbol}-${notification.type}: ${numToCurrency(notification.targetPrice)} USD\n`
+            listCoins += `${notification.coin.symbol}-${
+                notification.type
+            }: ${numToCurrency(notification.targetPrice)} USD\n`
         })
 
         bot.sendMessage(msg.chat.id, `${listCoins}`, { parse_mode: 'Markdown' })
     } else {
-        bot.sendMessage(msg.chat.id, `No notification added yet!`, { parse_mode: 'Markdown' })
+        bot.sendMessage(msg.chat.id, `No notification added yet!`, {
+            parse_mode: 'Markdown'
+        })
     }
-
-    
-    
 })
 
-
-
 bot.onText(/\/notify (.+)/, async (msg, match) => {
-    
-    const resp = match[1].split(" ");
+    const resp = match[1].split(' ')
     const notificationModel = new BaseRepository(Notification)
     const UserModel = await new BaseRepository(User)
     const coinModel = new BaseRepository(Coin)
 
-    const symbol = resp[0].split("-");
-   
-    const coins = await coinModel.find({symbol: symbol[0]})
-    const user = await UserModel.find({ username: msg.from.username })
-  
-    await notificationModel.save({type: symbol[1], targetPrice: resp[1], notificationChannel: 'telegram', coinId: coins.id, userId: user.id});
+    const symbol = resp[0].split('-')
 
-    bot.sendMessage(msg.chat.id, `Notification created successfully`, { parse_mode: 'Markdown' })
+    const coins = await coinModel.find({ symbol: symbol[0] })
+    const user = await UserModel.find({ username: msg.from.username })
+
+    await notificationModel.save({
+        type: symbol[1],
+        targetPrice: resp[1],
+        notificationChannel: 'telegram',
+        coinId: coins.id,
+        userId: user.id
+    })
+
+    bot.sendMessage(msg.chat.id, `Notification created successfully`, {
+        parse_mode: 'Markdown'
+    })
 })
 
+bot.onText(/\/delete (.+)/, async (msg, match) => {
+    const resp = match[1].split(' ')
+    const notificationModel = new BaseRepository(Notification)
+    const coinModel = new BaseRepository(Coin)
+
+    const symbol = resp[0].split('-')
+    const coins = await coinModel.find({ symbol: symbol[0] })
+
+    await notificationModel.delete({
+        where: {
+            type: symbol[1],
+            targetPrice: resp[1],
+            notificationChannel: 'telegram',
+            coinId: coins.id,
+        }
+    })
+
+    bot.sendMessage(msg.chat.id, `Notification deleted successfully`, {
+        parse_mode: 'Markdown'
+    })
+})
 
 bot.onText(/\/help/, async (msg) => {
-    
-    bot.sendMessage(msg.chat.id, `To set notifications first check for available coins and use their key as follows: \n\n To check when a coin goes below certain amount \n /notify {coinKey}-below amount \n\n To check when a coin goes above certain amount \n /notify {coinKey}-above amount \n\nExample, let's set a notification for when bitcoin goes below 32,000 USD\n/notify btc-below 32000 \n\n I hope you're good to go ;)`, { parse_mode: 'Markdown' })
+    bot.sendMessage(
+        msg.chat.id,
+        `To set notifications first check for available coins and use their key as follows: \n\n To check when a coin goes below certain amount \n /notify {coinKey}-below amount \n\n To check when a coin goes above certain amount \n /notify {coinKey}-above amount \n\nExample, let's set a notification for when bitcoin goes below 32,000 USD\n/notify btc-below 32000 \n\n I hope you're good to go ;)`,
+        { parse_mode: 'Markdown' }
+    )
 })
-
 
 // Get donate addresses
 bot.onText(/\/donate/, (msg, match) => {
-    let chatId = msg.chat.id;
+    let chatId = msg.chat.id
     bot.sendMessage(
-      chatId,
-      `Hello :) do you care so much about us?.\n\nCan send some coins to keep my developers happy and help make me better ❤️ \n\nBitcoin: *bc1q80dntcprztpwqhejeu6vwuxwp3wwaqk7l9qwaz* \n\nEthereum: *0x629817D3b3BfF27c047FDFDd879107FaDE3F4af7* \n\nERC20: *0x629817D3b3BfF27c047FDFDd879107FaDE3F4af7* \n\nBNB: *bnb1jckznag0mh6fqlss4g5qu5ywkdqgm5q4xs8c8z*`,
-      {
-        parse_mode: 'Markdown',
-      }
-    );
-  });
+        chatId,
+        `Hello :) do you care so much about us?.\n\nCan send some coins to keep my developers happy and help make me better ❤️ \n\nBitcoin: *bc1q80dntcprztpwqhejeu6vwuxwp3wwaqk7l9qwaz* \n\nEthereum: *0x629817D3b3BfF27c047FDFDd879107FaDE3F4af7* \n\nERC20: *0x629817D3b3BfF27c047FDFDd879107FaDE3F4af7* \n\nBNB: *bnb1jckznag0mh6fqlss4g5qu5ywkdqgm5q4xs8c8z*`,
+        {
+            parse_mode: 'Markdown'
+        }
+    )
+})
